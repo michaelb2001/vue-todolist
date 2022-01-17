d2fdaf31313963785ad121a3cf1d61bf3374579d
michaelb2001 <brunco.michael@gmail.com>
2022-01-17 15:43:12 +0100

    prima versione

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 let vm = new Vue({
     el:"#app",
     data: {
-        arrayObj: [
-
-        ]
+        isActive : false,
+        line : "text-decoration-line-trought text-dark",
+        arrayObj: []
     },
     methods:{
         aggiungiElemento : function(){
+            if(document.getElementById("aggiungi").value != ""){
+                let obj = {text : document.getElementById("aggiungi").value,
+                          done : false};
+                this.arrayObj.push(obj);
+                document.getElementById("aggiungi").value = "";
+            }
 
-            let obj = {text : document.getElementById("aggiungi").value,
-                       done : false};
-            this.arrayObj.push(obj);
-            document.getElementById("aggiungi").value = "";
         },
-        sottolinea : function(){
-            
-        }
+
+        cancellaElemento : function(index){
+            this.arrayObj.splice(index, 1);
+        },
+
+        toggleClass : function(index){
+     
+            this.arrayObj[index].done = !this.arrayObj[index].done;
+          }
+        
     }
 });
