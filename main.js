a1b5dba23b656428da18d4d01ccb3a7990bce0d2
michaelb2001 <brunco.michael@gmail.com>
2022-01-17 13:22:20 +0100

    files

---
+++ main.js
@@ -0,0 +1,23 @@
+let vm = new Vue({
+    el:"#app",
+    data: {
+        arrayObj: [
+            {
+                text:"primo",
+                done: false
+            },
+            {
+                text:"secondo",
+                done: false
+            },
+            {
+                text:"terzo",
+                done: false
+            },
+            {
+                text:"quarto",
+                done: false
+            }
+        ]
+    }
+});
